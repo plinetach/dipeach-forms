91fa1411c22087972d71edbdf9bd459d9cb761aa
ktsouvalis <67092537+ktsouvalis@users.noreply.github.com>
2021-01-29 10:11:30 +0200

    PDO to DbController

--- DbController.class.php
+++ DbController.class.php
@@ -2,10 +2,22 @@
 class DbController{
     
     function __construct(){
+    
+        $dns = "mysql:host=$hostname;dbname=$dbName";
+        $dbUsername = 'test';
+        $dbPassword = 'test';
+        try{
 
+            $db = new PDO($dns, $dbUsername, $dbPassword);
+          
+        }catch(Exception $e)
+        {
+            $errorMessage = $e->getMessage();
+            echo "<p>Error message: $errorMessage </p>";
+        }
     }
 
-    function checkCredentials($username,$password){
+    function checkCredentials($sessionUsername,$sessionPassword){
         
     }
 
