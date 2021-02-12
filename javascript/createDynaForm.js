95aba5105f2223d790d30b9ad8dbe7eee70df092
ktsouvalis <67092537+ktsouvalis@users.noreply.github.com>
2021-02-12 12:24:33 +0200

    021221 12.25

--- javascript/createDynaForm.js
+++ javascript/createDynaForm.js
@@ -1,27 +1,34 @@
 var numberOfElements = 1;
 
 function addField(){
+    var oldNumberOfElements = numberOfElements;
     numberOfElements++;
-    suffix = numberOfElements.toString();
-    newDivId = 'choices' + suffix;
-    newLabelText = suffix + 'ο Πεδίο';
-    newSelectId = 's' + suffix;
-    newSelectName = 's' + suffix;
-    newNewButtonId = 'bn' + suffix;
-    newRemoveButtonId = 'br' + suffix;
+    var suffix = numberOfElements.toString();
+    var newDivId = 'choices' + suffix;
+    var newLabelText = suffix + 'ο Πεδίο';
+    var newSelectId = 's' + suffix;
+    var newSelectName = 's' + suffix;
+    var newNewButtonId = 'bn' + suffix;
+    var newRemoveButtonId = 'br' + suffix;
 
-    $('#fields').append($("<br><div id='"+newDivId+"'></div>" ));
+    //$('#fields').append($("<br>" ));
+    $('#fields').append($("<div id='"+newDivId+"'></div>" ));
     $('#'+newDivId).append($("<label for='"+newSelectId+"'>"+newLabelText+"&nbsp;</label>"));
     $('#'+newDivId).append($("<select id='"+newSelectId+"' name="+newSelectName+"></select>"));
     $('#'+newSelectId).append($("<option value='1'>Πεδίο Κειμένου</option><option value='2'>Πεδίο NAI/OXI</option>"));
     $('#'+newDivId).append($("<button id='"+newNewButtonId+"' type='button' onclick='addField()'>+</button>"));
     $('#'+newDivId).append($("<button id='"+newRemoveButtonId+"' type='button' onclick='removeField()'>-</button>"));
+    $('#bn'+oldNumberOfElements.toString()).css("display", "none");
+    $('#br'+oldNumberOfElements.toString()).css("display", "none");
 
     console.log(numberOfElements);
 }
 
 function removeField(){
-   $('#choices'+ suffix).remove();
-   numberOfElements--;
-   console.log(numberOfElements);
+    var suffix = numberOfElements.toString();
+    $('#choices'+ suffix).remove();
+    numberOfElements--;
+    $('#bn'+numberOfElements.toString()).css("display", "inline");
+    $('#br'+numberOfElements.toString()).css("display", "inline");
+    console.log(numberOfElements);
 }
