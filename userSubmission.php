38e7dfa7468513ba948de309025a6880f536ff51
ktsouvalis <67092537+ktsouvalis@users.noreply.github.com>
2021-02-19 18:05:50 +0200

    .

--- userSubmission.php
+++ userSubmission.php
@@ -2,10 +2,10 @@
 include 'core/init.php';
 
 //-------------->TESTING DATA<--------------//
-// $user = "065105785";
+$user = "065105785";
 // $user = "029762769";
 // $user = "111111111";
-$user = "222222222"; 
+// $user = "222222222"; 
 //-------------->TESTING DATA<--------------//
 $page = new UiController;
 if(!Input::exists()){
